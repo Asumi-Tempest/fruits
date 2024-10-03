b1966cc14bc983a9b1869af938830bf4da47021c
WS41 <itzgeraldboi@gmail.com>
2024-10-03 09:22:02 +0800

    fruits

---
+++ fav-fruits.py
@@ -0,0 +1,28 @@
+validfruits=["banana","apple","orange","mango"]
+
+for G in validfruits:
+    fruits=input("enter fruits:")
+    if fruits == "banana":
+        print("hellow banana")
+    elif fruits == "apple":
+        print("hellow apple")
+    elif fruits == "orange":
+        print("no")
+    elif fruits == "mango":
+        print("no")
+    else:
+        print("bye")    
+
+    
+
+    
+
+
+    
+    
+    
+
+
+
+    
+  
---
+++ fav_fruits.py
@@ -0,0 +1,6 @@
+num=int(input("Number of fav_fruits :"))
+favfruits = []
+for G in range(num):
+    name=input("Enter your favorite fruits : ")
+    favfruits.append(name)
+    print(favfruits)
